099fa735a54690eb7ccd44fbfcebc4b68c39ab6a
Igaki12 <igakin8.19971222@gmail.com>
2024-11-24 18:55:12 +0900

    Update index.js

--- index.js
+++ index.js
@@ -601,7 +601,7 @@ videoSelector.addEventListener("change", async (event) => {
             console.log("Video duration : " + selectedVideo.duration);
             for (let timestamp = 0; timestamp < selectedVideo.duration * 1000; timestamp += 1000 / selectedVideo.fps){
                 const result = await poseLandmarker.detectForVideo(selectedVideo, timestamp);
-                if (result && result.landmarks && result.landmarks[0] && result.landmarks[0].length > 0) {
+                if (result && result.landmarks && result.landmarks[0]){
                     console.log("Video result at " + timestamp + "ms : ");
                     console.log(result);
                 }
